e45e6719dc3f2d39c88b49cea25170226fb38d6c
Janusz <mail.janusz@wp.pl>
2021-09-23 00:18:27 +0200

    Configuration commit #4 - basic folders structure created.

---
+++ src/main/java/com/kodilla/ecommercee/domain/exampleClass_everySingleClassDedicatedPackage/ExampleClass.java
@@ -0,0 +1,5 @@
+package com.kodilla.ecommercee.domain.exampleClass_everySingleClassDedicatedPackage;
+
+// Example class - to  remove before deployment (all package)
+public class ExampleClass {
+}
---
+++ src/main/java/com/kodilla/ecommercee/domain/exampleClass_everySingleClassDedicatedPackage/dao/ExampleClassDao.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee.domain.exampleClass_everySingleClassDedicatedPackage.dao;
+
+public class ExampleClassDao {
+}
