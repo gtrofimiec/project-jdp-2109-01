e2df66074c6f57bdbe9ac061084143845034e4e0
AleksanderKolompar <akolompar16@gmail.com>
2021-09-24 23:27:59 +0200

    Added GroupRepository

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,2 +1,25 @@
-package com.kodilla.ecommercee.repository;public interface GroupRepository {
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Group;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GroupRepository extends CrudRepository<Group, Long> {
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Group save(Group group);
+
+    @Override
+    Optional<Group> findById(Long group_id);
+
+    @Override
+    void deleteById(Long group_id);
+
+
 }
