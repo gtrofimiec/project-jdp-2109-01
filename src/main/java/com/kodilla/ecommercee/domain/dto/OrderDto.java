2805ba81d390bcef0374e00838569239855ff1e7
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2021-09-27 10:34:54 +0200

    OrderController implementation with OrderDto (task: JDP210901-4)

---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDto {
+
+    private BigDecimal totalPrice;
+}
